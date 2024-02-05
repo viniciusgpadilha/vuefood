f1da7df3975e09467f0ac696736cb4821c68deb2
viniciusgpadilha <vp2002@gmail.com>
2024-02-05 18:41:14 -0300

    instalando e configurando vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp, h } from 'vue'
 import BaseTemplate from './layouts/BaseTemplate'
 import router from './routes'
+import store from './store'
 
 const app = createApp(BaseTemplate)
 
 app.use(router)
+app.use(store)
 
 app.mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
+
+/* import { state, mutations } from './default'
+import companies from './modules/companies'
+import cart from './modules/cart/cart'
+import auth from './modules/auth/auth'
+import orders from './modules/orders/orders' */
+
+const store = createStore({
+/*     modules: {
+        companies,
+        cart,
+        auth,
+        orders,
+    },
+    state,
+    mutations */
+})
+
+export default store
